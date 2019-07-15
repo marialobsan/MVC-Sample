dc7b78c73905af5da374b6143468bca9ef255135
Anne Saints <annesaints@Annes-iMac.local>
2019-07-15 20:56:48 +0800

    View layer

--- MVC Proj/View/CustomPrettyView.swift
+++ MVC Proj/View/CustomPrettyView.swift
@@ -9,13 +9,16 @@
 import UIKit
 
 class CustomPrettyView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override func awakeFromNib() {
+        layer.cornerRadius = 20
+        layer.shadowColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        layer.shadowRadius = 10
+        layer.shadowOpacity = 0.75
+        backgroundColor = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
+        layer.borderColor = #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1)
+        layer.borderWidth = 5
+        
     }
-    */
+ 
 
 }
