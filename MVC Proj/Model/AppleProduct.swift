743f0aacaa963308497be9596617ea8be20fd96e
Anne Saints <annesaints@Annes-iMac.local>
2019-07-15 19:43:04 +0800

    Model layer

---
+++ MVC Proj/Model/AppleProduct.swift
@@ -0,0 +1,21 @@
+//
+//  AppleProduct.swift
+//  MVC Proj
+//
+//  Created by Anne Saints on 15/07/2019.
+//  Copyright © 2019 Anne Saints. All rights reserved.
+//
+
+import Foundation
+class AppleProduct {
+    var name: String
+    var color: String
+    var price: Double
+    
+    init(name: String, color: String, price: Double) {
+        self.name = name
+        self.color = color
+        self.price = price
+        
+    }
+}
